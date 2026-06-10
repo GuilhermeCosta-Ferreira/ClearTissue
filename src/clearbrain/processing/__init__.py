666eaf5e8ce3b7ea1b67e26b43199e06fc30e1e4
GuilhermeCF <guicostaferreira.2003@gmail.com>
2026-06-10 13:34:29 +0200

    fix: is now possible to select where to start and end sample

--- src/clearbrain/processing/__init__.py
+++ src/clearbrain/processing/__init__.py
@@ -1,19 +1,24 @@
 from .scale import scale_tissue
-from .downsample import compress_to_volume
+from .downsample import compress_to_volume, build_volume
 from .centerline import get_centerline
 from .stretch import stretch_tissue
-from .untwist import untwist_spinal_coord
+from .untwist import untwist_spinal_coord, apply_know_untwisting
 from .noise import clear_external_points
 from .rotate import rotate_spinal_cord
-from .crop import crop_excess
+from .crop import crop_excess, apply_crop_excess
+from .sample_clamp import crop_spinal_cord
 
 __all__ = [
     "scale_tissue",
     "compress_to_volume",
+    "build_volume",
     "get_centerline",
     "stretch_tissue",
     "untwist_spinal_coord",
+    "apply_know_untwisting",
     "clear_external_points",
     "rotate_spinal_cord",
     "crop_excess",
+    "apply_crop_excess",
+    "crop_spinal_cord"
 ]
