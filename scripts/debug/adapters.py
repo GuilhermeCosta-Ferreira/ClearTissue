d37c3cd5a3ec22a717f0b3326143962b7696bd76
GuilhermeCF <guicostaferreira.2003@gmail.com>
2026-06-18 17:31:39 +0200

    fix: normal usage

--- scripts/debug/adapters.py
+++ scripts/debug/adapters.py
@@ -1,13 +1,10 @@
 # ================================================================
 # 0. Section: IMPORTS
 # ================================================================
-from copy import deepcopy
-
 from clearbrain.domain_model.data import TissueType
 from clearbrain.service.ClearTissueProject import ClearTissueProject
 import clearbrain.domain_model.transformations as tr
 
-from clearbrain.registration import Registrator, RigidRegistration, RegistratorResampler, RegistrationConfig
 
 
 
@@ -25,33 +22,15 @@ if __name__ == '__main__':
     )
     raw_batch = project.load_raw()
 
-    pipeline = project.init_pipeline()
+    pipeline = project.init_pipeline("debug the new config")
 
-    tissue_registrator = Registrator(
-        strategy=RigidRegistration(),
-        resampler=RegistratorResampler(),
-        config=RegistrationConfig(),
-    )
-    tissue_registrator.config.metric.name = "CC"
-    tissue_registrator.config.metric.sampling_percentage = 1
-    tissue_registrator.config.metric.histogram_bins = 50
-    tissue_registrator.config.optimizer.iterations = 500
-    tissue_registrator.config.optimizer.convergence_minimum_value = 1e-8
-    tissue_registrator.config.optimizer.convergence_window_size = 30
-
-    cell_registrator = deepcopy(tissue_registrator)
-    cell_registrator.config.interpolator.resampling = "nearest"
+    input("Setup the config. Press enter when ready")
 
     pipeline.add_list([
-        tr.RegularizeSample(),
-        tr.OrientSample(),
-        tr.StretchSample(smooth_window_size=25),
-        tr.UntwistSample(
-            tissue_registrator=tissue_registrator,
-            cell_registrator=cell_registrator,
-            window_size=250,
-            gap_size=0
-        )
+        tr.RegularizeSample,
+        tr.OrientSample,
+        tr.StretchSample,
+        tr.UntwistSample,
     ])
 
     final_batch = project.run_pipeline(pipeline, raw_batch)
